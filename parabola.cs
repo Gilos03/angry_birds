1dd7842906d89b92a5485f8757da09489fadeb96
Gilos03 <118567681+Gilos03@users.noreply.github.com>
2024-02-19 13:55:49 +0500

    com3

--- parabola.cs
+++ parabola.cs
@@ -7,7 +7,7 @@ using System.IO;
 
 namespace WpfApp
 {
-    class ParapolaFlight
+    class ParabolaFlight
     {
         readonly double V0;
         readonly double alpha;
@@ -16,7 +16,7 @@ namespace WpfApp
         public List<Point_and_Velocity> Points = new List<Point_and_Velocity>;
         double splitting = 1;
 
-        public ParapolaFlight(double V0, double alpha, double weight, double splitting)
+        public ParabolaFlight(double V0, double alpha, double weight, double splitting)
         {
             this.V0 = V0;
             this.alpha = alpha;
@@ -35,12 +35,62 @@ namespace WpfApp
             return new Point_and_Velocity(x, y, Vx, Vy);
         }
 
-        public void calculation(double x0, double y0)
+        public void Calculation(double x0, double y0)
         {
             double t = 0;
-            double T = 2 * V0 * Math.sin(alpha) / g;
+            double T = 2 * V0 * Math.Sin(alpha) / g;
+            double dt = T/splitting;
 
+            Point_and_Velocity pv = new Point_and_Velocity(x0, y0, V0 * Math.Cos(alpha), V0 * Math.Sin(alpha));
+            Points.Add(pv);
+            t += dt;
+            Int32 i = 1;
+
+            while(t < T)
+            {
+                double k = 1/2;
+                Point_and_Velocity get_pos = GetPosition(Points, i, dt, weight, k);
+                Points.Add(getpos);
+                t += dt;
+                i += 1;
+            }
         }
     }
+        public void Read(string path)
+        {
+            using(StreamReader sr = new StreamReader(path))
+            {
+                string s;
 
+                while((s = sr.ReadLine() != null)
+                {
+                    string[] subs = s.Split();
+
+                    if(!double.TryParse(double[0], out double X)
+                    {
+                        return;
+                    }
+
+                    if(!double.TryParse(double[1], out double Y)
+                    {
+                        return;
+                    }
+
+                    Point_and_Velocity p = new Point_and_Velocity(X, Y, 0, 0);
+                    Points.Add(p);
+                    }
+                }
+            }
+
+            public async void Write(string path)
+            {
+                using(StreamWriter outputFile = new StreamReader)
+                {
+                    foreach(Point_and_Velocity p in Points)
+                    {
+                        await outputFile.WriteLineAsync($"{p.x}, {p.y}");
+                    }
+                }
+            }
+        }
 }
